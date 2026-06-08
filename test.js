6048bef3ada4b447ca5d55e1b946c9ff2977d84b
tangostin <tangostin@gmail.com>
2026-06-08 23:31:30 +0900

    add test.js

---
+++ test.js
@@ -0,0 +1,15 @@
+const { chromium } = require('playwright');
+
+(async () => {
+  const browser = await chromium.launch({ headless: true });
+  const page = await browser.newPage();
+
+  await page.goto(
+    'https://kouen.sports.metro.tokyo.lg.jp/web/rsvWOpeInstSrchVacantAction.do',
+    { waitUntil: 'networkidle' }
+  );
+
+  console.log(await page.title());
+
+  await browser.close();
+})();
