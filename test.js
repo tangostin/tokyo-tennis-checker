5c867de9491b6bab9ae651bc0d3b67af94b35e85
tangostin <tangostin@gmail.com>
2026-06-09 00:29:53 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,40 +1,27 @@
 const { chromium } = require('playwright');
 
 (async () => {
-  console.log('START');
-
   const browser = await chromium.launch({ headless: true });
   const page = await browser.newPage();
 
-  page.on('response', async (response) => {
-    const url = response.url();
-
-    if (url.includes('rsvWOpeInstSrchVacantAjaxAction.do')) {
-      console.log('FOUND API');
-      console.log(url);
-    }
-  });
+  console.log('START');
 
   await page.goto('https://kouen.sports.metro.tokyo.lg.jp/web/', {
     waitUntil: 'networkidle'
   });
 
-  console.log('TOP PAGE LOADED');
-
   await page.selectOption('#purpose-home', '1000_1030');
-  console.log('PURPOSE SELECTED');
-
   await page.waitForTimeout(2000);
 
   await page.selectOption('#bname-home', '1040');
-  console.log('PARK SELECTED');
-
   await page.waitForTimeout(2000);
 
   await page.click('#btn-go');
-  console.log('SEARCH CLICKED');
 
-  await page.waitForTimeout(10000);
+  await page.waitForTimeout(5000);
+
+  console.log('URL=' + page.url());
+  console.log('TITLE=' + await page.title());
 
   console.log('END');
 
