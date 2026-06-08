6d54b675dfb81b69a5f55846b8dc6771caf9d961
tangostin <tangostin@gmail.com>
2026-06-09 00:37:39 +0900

    Update test.js

--- test.js
+++ test.js
@@ -7,16 +7,23 @@ const { chromium } = require('playwright');
   page.on('response', async (response) => {
     const url = response.url();
 
-    if (
-      url.includes('AjaxAction')
-    ) {
-      console.log('AJAX=' + url);
+    if (url.includes('rsvWOpeInstSrchVacantAjaxAction.do')) {
+      console.log('FOUND');
+
+      try {
+        const text = await response.text();
+
+        console.log(text.substring(0, 3000));
+      } catch (e) {
+        console.log(e.toString());
+      }
     }
   });
 
-  await page.goto('https://kouen.sports.metro.tokyo.lg.jp/web/', {
-    waitUntil: 'networkidle'
-  });
+  await page.goto(
+    'https://kouen.sports.metro.tokyo.lg.jp/web/',
+    { waitUntil: 'networkidle' }
+  );
 
   await page.selectOption('#purpose-home', '1000_1030');
   await page.waitForTimeout(2000);
@@ -27,8 +34,6 @@ const { chromium } = require('playwright');
   await page.click('#btn-go');
 
   await page.waitForTimeout(15000);
-
-  console.log('URL=' + page.url());
 
   await browser.close();
 })();
